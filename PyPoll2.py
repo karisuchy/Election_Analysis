4560a6253efeb7c46565cf1d8fd82cadbbcb9aa5
Kari Suchy <90162669+karisuchy@users.noreply.github.com>
2021-10-09 12:36:37 -0500

    Add files via upload
    Adding the election audit code

--- PyPoll2.py
+++ PyPoll2.py
@@ -17,6 +17,11 @@ candidate_options = []
 # Declare the empty dictionary.
 candidate_votes = {}
 
+#Winning candidate and winning count tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -50,8 +55,24 @@ for candidate_name in candidate_votes:
     votes = candidate_votes[candidate_name]    
     # Calculate the percentage of votes.
     vote_percentage = float(votes) / float(total_votes) * 100
+    
+    # Determine winning vote count and candidate
+    # Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # if true, then set winning_count = votes and winning_percent = vote percentage
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # Set the winning_candidate equal to the candidate's name
+        winning_candidate = candidate_name 
+
     # Print the candidate name and percentage of votes.
-    print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.")
-
-
-
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,}.)\n")
+    
+# print winning candidate summary
+winning_candidate_summary = (
+    f" ---------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}% \n"
+    f"----------------------\n")
+print(winning_candidate_summary)
